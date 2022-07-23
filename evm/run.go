45af69dd89b57d1c536afcd8396135e3cd16b84d
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-07-23 12:34:49 -0300

    stop when program counter is outside of code scope

--- evm/run.go
+++ evm/run.go
@@ -18,6 +18,11 @@ func Run(code []byte) {
 }
 
 func decodeOpcode(execCtx *ExecutionContext) *Instruction {
+	// section 9.4.1 of the yellow paper, the operation to be executed if pc is outside code is STOP
+	if execCtx.pc >= len(execCtx.code) {
+		return STOP
+	}
+
 	opcode := Opcode(execCtx.ReadCode(1)[0])
 	ix := InstructionsByOpcode[opcode]
 	if ix == nil {

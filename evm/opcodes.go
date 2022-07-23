b099c8845d84e025a0ee69225d7ef8dd9cb7cb69
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-07-23 17:17:25 -0300

    add missing dups and swaps

--- evm/opcodes.go
+++ evm/opcodes.go
@@ -81,10 +81,118 @@ var DUP3 = registerInstruction(0x82, "DUP3", func(ctx *ExecutionContext) {
 	ctx.stack.push(ctx.stack.peek(3))
 })
 
-// TODO: Implement until DUP16
+var DUP4 = registerInstruction(0x83, "DUP4", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(4))
+})
+
+var DUP5 = registerInstruction(0x84, "DUP5", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(5))
+})
+
+var DUP6 = registerInstruction(0x85, "DUP6", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(6))
+})
+
+var DUP7 = registerInstruction(0x86, "DUP7", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(7))
+})
+
+var DUP8 = registerInstruction(0x87, "DUP8", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(8))
+})
+
+var DUP9 = registerInstruction(0x88, "DUP9", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(9))
+})
+
+var DUP10 = registerInstruction(0x89, "DUP10", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(10))
+})
+
+var DUP11 = registerInstruction(0x8a, "DUP11", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(11))
+})
+
+var DUP12 = registerInstruction(0x8b, "DUP12", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(12))
+})
+
+var DUP13 = registerInstruction(0x8c, "DUP13", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(13))
+})
+
+var DUP14 = registerInstruction(0x8d, "DUP14", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(14))
+})
+
+var DUP15 = registerInstruction(0x8e, "DUP15", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(15))
+})
+
+var DUP16 = registerInstruction(0x8f, "DUP16", func(ctx *ExecutionContext) {
+	ctx.stack.push(ctx.stack.peek(16))
+})
 
 var SWAP1 = registerInstruction(0x90, "SWAP1", func(ctx *ExecutionContext) {
 	ctx.stack.swap(1)
 })
 
-// TODO: Implement until SWAP16
+var SWAP2 = registerInstruction(0x91, "SWAP2", func(ctx *ExecutionContext) {
+	ctx.stack.swap(2)
+})
+
+var SWAP3 = registerInstruction(0x92, "SWAP3", func(ctx *ExecutionContext) {
+	ctx.stack.swap(3)
+})
+
+var SWAP4 = registerInstruction(0x93, "SWAP4", func(ctx *ExecutionContext) {
+	ctx.stack.swap(4)
+})
+
+var SWAP5 = registerInstruction(0x94, "SWAP5", func(ctx *ExecutionContext) {
+	ctx.stack.swap(5)
+})
+
+var SWAP6 = registerInstruction(0x95, "SWAP6", func(ctx *ExecutionContext) {
+	ctx.stack.swap(6)
+})
+
+var SWAP7 = registerInstruction(0x96, "SWAP7", func(ctx *ExecutionContext) {
+	ctx.stack.swap(7)
+})
+
+var SWAP8 = registerInstruction(0x97, "SWAP8", func(ctx *ExecutionContext) {
+	ctx.stack.swap(8)
+})
+
+var SWAP9 = registerInstruction(0x98, "SWAP9", func(ctx *ExecutionContext) {
+	ctx.stack.swap(9)
+})
+
+var SWAP10 = registerInstruction(0x99, "SWAP10", func(ctx *ExecutionContext) {
+	ctx.stack.swap(10)
+})
+
+var SWAP11 = registerInstruction(0x9A, "SWAP11", func(ctx *ExecutionContext) {
+	ctx.stack.swap(11)
+})
+
+var SWAP12 = registerInstruction(0x9B, "SWAP12", func(ctx *ExecutionContext) {
+	ctx.stack.swap(12)
+})
+
+var SWAP13 = registerInstruction(0x9C, "SWAP13", func(ctx *ExecutionContext) {
+	ctx.stack.swap(13)
+})
+
+var SWAP14 = registerInstruction(0x9D, "SWAP14", func(ctx *ExecutionContext) {
+	ctx.stack.swap(14)
+})
+
+var SWAP15 = registerInstruction(0x9E, "SWAP15", func(ctx *ExecutionContext) {
+	ctx.stack.swap(15)
+})
+
+var SWAP16 = registerInstruction(0x9F, "SWAP16", func(ctx *ExecutionContext) {
+	ctx.stack.swap(16)
+})
